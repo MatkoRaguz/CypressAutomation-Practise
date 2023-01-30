dbc4ee3c5cd55ec2b2c1c14ffb9658fbf7dd64e3
MatkoRaguz <matkoraguz1@gmail.com>
2023-01-30 18:50:30 +0100

    yes

--- cypress/integration/examples/ClickAcademy.js
+++ cypress/integration/examples/ClickAcademy.js
@@ -4,7 +4,7 @@
 describe('ClickAcademy', () => {
 
 
-    it('Checkboxes', () => {
+    it('Checkboxes, Dropdowns, Hide and visible', () => {
         
         cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/");
 
@@ -12,6 +12,12 @@ describe('ClickAcademy', () => {
         cy.get("#checkBoxOption1").check().should("be.checked").and("have.value", "option1")
         cy.get("#checkBoxOption1").uncheck().should("not.be.checked")
         cy.get('input[type="checkbox"]').check(["option2", "option3"])
+
+
+        //Radio buttons
+        cy.get("[value='radio3']").check().should("be.checked")
+        cy.get("[value='radio1']").check().should("be.checked")
+        cy.get("[value='radio2']").check().should("be.checked")
 
 
         //Dropdowns interaction
@@ -28,10 +34,19 @@ describe('ClickAcademy', () => {
                 cy.wrap($el).click()
             }
         // cy.get("#autocomplete").should("have.value", "Switzerland")
-        
+
         })
 
+        // Handling Visible and invisible elements using Assertions in Cypress
+        cy.get("#displayed-text").should("be.visible")
+        cy.get("#hide-textbox").click()
+        cy.get("#displayed-text").should("not.be.visible")
+        cy.get("#show-textbox").click()
+        cy.get("#displayed-text").should("be.visible")
+
     });
+
+
 
     
 });
